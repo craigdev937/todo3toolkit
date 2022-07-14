cac01cef3148ad9cacb8bcb13863bb9e1f5defb2
Craig <indigomx9@gmail.com>
2022-07-14 16:30:16 -0400

    Added TailwinCSS

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 export const App = () => {
     return (
         <React.Fragment>
-            <h1>App</h1>
+            <h1 
+                className="text-violet-900 
+                    text-2xl"
+                >TailwindCSS
+            </h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci fugiat excepturi tempore officiis suscipit commodi veniam voluptates nulla esse ad! Quis asperiores aspernatur animi corrupti officiis consectetur dolores vel molestias.</p>
         </React.Fragment>
     );

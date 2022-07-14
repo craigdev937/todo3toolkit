0858e80af4248d08a68661b7f6b2b5d5c1da5691
Craig <indigomx9@gmail.com>
2022-07-14 16:18:13 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Redux-Toolkit
+
+Initial setup of the project.  Created with JSON Server, React, Fetch API, React Router, and Redux-Toolkit.
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci fugiat excepturi tempore officiis suscipit commodi veniam voluptates nulla esse ad! Quis asperiores aspernatur animi corrupti officiis consectetur dolores vel molestias.</p>
+        </React.Fragment>
+    );
+};
+
+
